13bab25f7c00bb84b581c76d65ea01d7c43dcb2b
L <121633229+hunroi@users.noreply.github.com>
2023-09-26 17:41:44 +0200

    Update file-scr.js

--- file-scr.js
+++ file-scr.js
@@ -7,7 +7,7 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
 
   // Manipulate the file content, convert to uppercase letters
   const modifiedData = data.toUpperCase();
-  // const modifiedData = data.LowerCase();
+  const modifiedData = data.LowerCase();
   
   // Write the modified content to the new file
   fs.writeFile('output.txt', modifiedData, (err) => {
